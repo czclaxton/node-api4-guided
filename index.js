e53e9009326609a49963346b19fe3bbc2f964101
Connor Claxton <czclaxton@gmail.com>
2019-09-05 13:09:13 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
-const defaults = require("./config/defaults");
+// const defaults = require("./config/defaults");
 
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 4000;
+console.log(process.env.PORT);
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
